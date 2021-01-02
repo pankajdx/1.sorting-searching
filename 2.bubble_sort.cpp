6458e218755ce6bb9a19a54e9a4cea3d3f3beeb4
pankajdx <pankaj.dx@gmail.com>
2021-01-02 15:40:12 +0530

    Added Bubble Sort

---
+++ 2.bubble_sort.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void bubble_sort(int *arr, int n) {
+	for (int i = 0; i < n - 1; ++i) {
+		for (int j = 0; j + 1 < n - i; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int n; cin >> n;
+	int *arr = new int[n];
+
+	for (int i = 0; i < n; ++i)
+		cin >> arr[i];
+
+	bubble_sort(arr, n);
+
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << ' ';
+}
